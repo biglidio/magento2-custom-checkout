e562971000b460837da65cd200a227e1b1ccb244
Lucas Figueiredo <lucas-fj@hotmail.com>
2021-10-07 13:48:14 +0200

    #5 - Create UI Component & Knockout template in the checkout payment area

---
+++ view/frontend/web/js/view/payment/po-number.js
@@ -0,0 +1,9 @@
+define(['uiComponent'], function(Component) {
+    'use strict'
+
+    return Component.extend({
+        defaults: {
+            template: 'Biglidio_PoNumber/payment/po-number'
+        }
+    });
+});
